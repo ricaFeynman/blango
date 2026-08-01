d6c8c6e63fe329167b7eeef53500055845d8010a
codio <romualdGyorgy@gmail.com>
2026-08-01 09:03:36 +0300

    2:42 viewsets routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,12 +4,12 @@ from drf_yasg.views import get_schema_view
 import os
 
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
+
+from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
@@ -25,9 +25,14 @@ schema_view = get_schema_view(
     public=True,
 )
 
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token),
+    # path("token-auth/", views.obtain_auth_token),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
@@ -38,4 +43,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
