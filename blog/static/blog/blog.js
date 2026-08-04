b4c2192702d8eede955d9ee3afce714d934afc8c
codio <romualdGyorgy@gmail.com>
2026-08-04 15:15:46 +0300

    3:43 promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,59 +1,25 @@
-/*
-console.time('myTimer')
-console.count('counter1')
-console.log('A normal log message')
-console.warn('Warning: something bad might happen')
-console.error('Something bad did happen!')
-console.count('counter1')
-console.log('All the things above took this long to happen:')
-console.timeEnd('myTimer')
-*/
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
+}
 
-/*
-function sayHello(yourName) {
-  if (yourName === undefined) {
-      console.log('Hello, no name')
-  } else {
-       console.log('Hello, ' + yourName)
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
   }
+
+  return new Promise(doAdd)
 }
 
-const yourName = 'Your Name'  // Put your name here
+const p = lazyAdd(3, 4)
+p.then(resolvedCallback, rejectedCallback)
 
-console.log('Before setTimeout')
-
-setTimeout(() => {
-    sayHello(yourName)
-  }, 2000
-)
-
-console.log('After setTimeout')
-*/
-
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
-}
-
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
-}
-
-let k = 10
-
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
